40c04c7dd7dbc8381abab24baf2b40bdfd04b552
candy <448932728@qq.com>
2023-11-05 11:26:41 +0800

    #9 feat: 新增redis配置

--- internal/svc/service_context.go
+++ internal/svc/service_context.go
@@ -3,18 +3,22 @@ package svc
 import (
 	"rank-master-back/internal/config"
 	"rank-master-back/internal/pkg/orm_engine"
+	"rank-master-back/internal/pkg/rdb"
 
+	"github.com/go-redis/redis/v8"
 	"gorm.io/gorm"
 )
 
 type ServiceContext struct {
 	Config config.Config
 	DB     *gorm.DB
+	RDB    *redis.Client
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
 		DB:     orm_engine.NewGormEngine(c.DataSource),
+		RDB:    rdb.NewRdbClient(c.Redis),
 	}
 }
