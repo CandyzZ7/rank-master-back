23c02434869cc68e75530eb576a5baec0c60e49f
candy <448932728@qq.com>
2024-06-11 09:40:45 +0800

    feat: 调整 自定义错误码

--- infrastructure/response/response.go
+++ infrastructure/response/response.go
@@ -17,7 +17,7 @@ type Body struct {
 }
 
 // Handler 统一返回入口，
-func Handler(w http.ResponseWriter, resp interface{}, err error) {
+func Handler(w http.ResponseWriter, resp any, err error) {
 	if err != nil {
 		httpx.OkJson(w, e.ErrHandler(err))
 		// 如果err不为空的话，走错误处理函数，将err传递过去
