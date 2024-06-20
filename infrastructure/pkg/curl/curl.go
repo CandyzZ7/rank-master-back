374572ac027345e5a0408f3ec3fe190ca372f41c
tangyejian <tangyejian@zhonglian.com>
2024-06-20 11:01:18 +0800

    feat: 新增curl包

---
+++ infrastructure/pkg/curl/curl.go
@@ -0,0 +1,71 @@
+package curl
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+const (
+	AllowMethodGet  = "GET"
+	AllowMethodPost = "POST"
+	AllowMethodPut  = "PUT"
+)
+
+type Curl struct {
+	host    string
+	request *http.Request
+	data    io.Reader
+	method  string
+}
+
+func New(host string) *Curl {
+	return &Curl{
+		host:   host,
+		method: AllowMethodGet,
+	}
+}
+
+func (c *Curl) Request(v interface{}, headers ...HeaderHandlers) (err error) {
+	c.request, err = http.NewRequest(c.method, c.host, c.data)
+	if err != nil {
+		return err
+	}
+	for _, header := range headers {
+		header(c)
+	}
+	res, err := (&http.Client{}).Do(c.request)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var bs []byte
+	bs, err = io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bs, v)
+	if err != nil {
+		if res.StatusCode != 200 {
+			return errors.New(res.Status)
+		}
+		return err
+	}
+	return nil
+}
+
+func (c *Curl) Get() *Curl {
+	c.method = AllowMethodGet
+	return c
+}
+
+func (c *Curl) Post() *Curl {
+	c.method = AllowMethodPost
+	return c
+}
+
+func (c *Curl) Put() *Curl {
+	c.method = AllowMethodPut
+	return c
+}
